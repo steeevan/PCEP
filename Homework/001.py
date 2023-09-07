bc9e67d5c728b1d612ed54d76f44a0b09b37468c
Maybe-Michael <143658710+Maybe-Michael@users.noreply.github.com>
2023-09-06 17:13:56 -0700

    Hw of michael
    E

---
+++ Homework/001.py
@@ -0,0 +1,57 @@
+#Task 1
+integer_var = 1
+float_var = 2.0
+print("Task 1")
+print(integer_var+float_var)
+print("------------------------------------------------------------------------------------")
+
+#Task 2
+long_var = 12**14
+complex_var = (3**5//2+(13))
+print("Task 2")
+print(long_var*complex_var)
+print("------------------------------------------------------------------------------------")
+
+#Task 3
+string_var = "Michael Fan"
+sliced_string_var =(string_var[0:7])
+print("Task 3")
+print(sliced_string_var,", welcome to the Python class!")
+print("------------------------------------------------------------------------------------")
+
+#Task 4
+num_list = [1,2,3,4,5.0]
+len_list=(len(num_list))
+print("Task 4")
+print(len_list)
+print("------------------------------------------------------------------------------------")
+
+#Task 5
+fruit_tuple = {"apple", "bannana", "pear"}
+print(fruit_tuple)
+#error - TypeError: 'set' object is not subscriptable
+#you can't set a tupple to anything
+print("Task 5")
+print("------------------------------------------------------------------------------------")
+
+#Task 6
+student_dict = ["Name":"Michael", "Age": 12, "Grade": "6th"]
+print("Task 5")
+print(student_dict[1])
+print("------------------------------------------------------------------------------------")
+
+#Task 7
+bool_var1 = True
+bool_var2 = False
+print(bool_var1,(bool_var2))
+
+
+
+
+
+
+
+
+
+
+
