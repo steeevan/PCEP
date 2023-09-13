2164e1796dba0c26e3b1f900a968dcbcdc4bd4a3
ImagineDragon1956 <143158633+ImagineDragon1956@users.noreply.github.com>
2023-09-13 08:27:05 -0700

    Create Python HW 2.py

---
+++ Homework/Python HW 2.py
@@ -0,0 +1,88 @@
+age = 13
+type(age)
+print(type)
+
+length = 10
+width = 5
+area = length*width
+print(area)
+
+num1 = 15
+num2 = 20
+num1 < num2
+print(num1 < num2)
+
+#var = count
+#'count' = += 3
+#'count' =  *= 2
+#print(var)
+#idk
+
+name = "Caden"
+len(name)
+print(len(name))
+
+notebook_price = 2.50
+pencil_price = 0.75
+num_notebooks = 8
+num_pencils = 5
+total_notebook_cost = notebook_price*num_notebooks
+total_pencil_cost = pencil_price*num_pencils
+total = total_notebook_cost + total_pencil_cost
+print(total)
+
+fruit1 = "apple"
+fruit2 = "banana"
+fruit1 != fruit2
+print(fruit1 != fruit2)
+
+distance = 100
+distance -= 20
+distance **= 2
+print(distance)
+
+is_happy = True
+int(is_happy)
+print(int(is_happy))
+
+score1 = 85
+score2 = 52
+score3 = 78
+total_score = 85+52+78
+average_score = total_score/3
+print(average_score)
+
+sum_result = 10 +15
+product_result = 5 * 5
+sum_result >= product_result
+print(sum_result >= product_result)
+
+value = 7
+value *= 4
+value += 12
+print(value)
+
+temperature = 98.6
+str(temperature)
+print(str(temperature))
+
+total_amount = 500
+num_friends = 3
+amount_per_friend = total_amount/num_friends
+remaining_amount = total_amount%num_friends
+print(amount_per_friend)
+print(remaining_amount)
+
+product_result = 8 * -2
+abs_value = abs(product_result)
+print(abs_value)
+
+
+
+
+
+
+
+
+
+
