ff721be77fcbfeb9dc47b1979b299986fff5a708
japermousy <143354745+japermousy@users.noreply.github.com>
2023-09-13 18:20:29 -0700

    Create h2.py

---
+++ Homework/h2.py
@@ -0,0 +1,42 @@
+integer_var = 100
+float_var = 1.1
+print(integer_var+float_var)
+
+long_var = 10^10
+complex_var = 3 + 4j
+print(long_var)
+print(complex_var)
+
+string_var = "Jasper C Wang"
+print(string_var[0:6])
+string_var2 = "Jasper, welcome to the Python class!"
+print(string_var2)
+
+num_list = [1,2,3,4,5]
+num_list.append(1.2)
+print(num_list)
+
+fruit_tuple = ("kiwi","strawberry","watermelon")
+print(fruit_tuple[2])
+
+student_dict = {"name":"Jasper","age":13,"grade":"8th","city":"Chino Hills"}
+print(student_dict["age"])
+
+bool_var1 = True
+bool_var2 = False
+print(bool_var1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+student
