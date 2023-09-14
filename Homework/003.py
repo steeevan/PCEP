6844c350f2ba9d8f1bd227d3128e2b77aadfc275
Maybe-Michael <143658710+Maybe-Michael@users.noreply.github.com>
2023-09-13 17:56:58 -0700

    Add files via upload

---
+++ Homework/003.py
@@ -0,0 +1,94 @@
+#prob 1
+age = 12
+print(type(age))
+
+#prob 2
+length = 10
+width = 5
+area = length * width
+print(area)
+
+#prob 3
+if 15<20:
+    print("yes")
+else:
+    print("")
+
+#prob 4 
+count = 1
+
+count += 3
+count *= 2
+print(count)
+
+#prob 5
+name = "Michael"
+print(len(name))
+
+#prob 6
+notebook_price = 2.50
+pencil_price = 0.75
+num_notebooks = 8
+num_pencils = 5
+total_notebook_price = notebook_price*num_notebooks
+total_pencil_price = pencil_price*num_pencils
+total_cost = total_pencil_price + total_notebook_price
+print(total_cost)
+
+#prob 7
+TOF = 'apple' == 'banana'
+print (TOF)
+
+#prob 8
+distance = 100
+distance -= 20
+distance **= 2
+print(distance)
+
+#prob 9
+is_happy = True
+print(int(is_happy))
+
+#prob 10
+score1 = 85
+score2 = 92
+score3= 78
+total_score = score1 + score2 + score3
+avrage_score = total_cost/3
+print(avrage_score)
+
+#prob 11
+sum_result = 10+15
+product_result = 5*5
+TOF2 = sum_result >= product_result
+print(TOF2)
+
+#prob 12
+value = 7
+value *=4
+value +=12
+print(value)
+
+#prob 13
+temperature = 98.6
+print(str(temperature))
+
+#prob 14
+total_ammount = 500
+num_friends = 3
+amount_per_friend = total_ammount/num_friends
+print(amount_per_friend)
+
+#prob 15
+product_result = 8*-2
+abs_value = abs(-16)
+TOF3 = product_result < product_result
+print(TOF3)
+
+
+
+
+
+
+
+
