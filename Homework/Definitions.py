9facd5b5ae9998e5c04adae2bbeee8f2e3e94752
ImagineDragon1956 <143158633+ImagineDragon1956@users.noreply.github.com>
2023-10-04 15:42:29 -0700

    Create Definitions.py

---
+++ Homework/Definitions.py
@@ -0,0 +1,18 @@
+Python
+Tuple
+Syntax
+Loop
+List
+Float
+String
+Data Types
+Boolean
+Function
+Library
+Variable
+Integer
+Exception Handling
+Conditional Statements
+Tuple
+Interpreter
+Assignment Operator
