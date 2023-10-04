02caf129f54f43c7147b83518a0472d4693f3656
ImagineDragon1956 <143158633+ImagineDragon1956@users.noreply.github.com>
2023-10-04 15:31:15 -0700

    Create Python HW 3.py

---
+++ Homework/Python HW 3.py
@@ -0,0 +1,34 @@
+
+#1
+n = 1
+n = (int(input("Value")))
+sum = 0
+for i in range(1, n+1):
+    print(i)
+    if (1%2==0):
+        sum += i
+print("Sum")
+print(sum)
+
+#2
+
+n = (int(input("Value")))
+
+if n <=3:
+    for i in range(n-2):
+        for i in range(2):
+            print(aw)
+for i in range(n-1):
+        for i in range(n):
+            print("yay")
+
+
+#3
+
+n = (int(input("Value")))
+factorial = 1
+for i in range(1, n+1):
+    print(i)
+    factorial += 1
+
+print("Factorial")
